74365c12f90e52ab7185d494077db901f5ec4387
johnteh001 <133164691+johnteh001@users.noreply.github.com>
2024-04-17 07:23:33 +0000

    set up sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,24 @@
--- Script prepares MySQL server for the project
--- database hbnb_test_db is created
--- A new user hbnb_test is created in the localhost
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Check if the database hbnb_dev_db already exists
+SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'hbnb_dev_db';
+
+-- If it doesn't exist, create it
+IF @@rowcount = 0 THEN
+  CREATE DATABASE hbnb_dev_db;
+END IF;
+
+-- Check if the user hbnb_dev@localhost already exists
+SELECT User, Host FROM mysql.user WHERE User = 'hbnb_dev' AND Host = 'localhost';
+
+-- If it doesn't exist, create it with password
+IF @@rowcount = 0 THEN
+  CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+END IF;
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev@localhost
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev@localhost
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to ensure immediate effect
 FLUSH PRIVILEGES;
